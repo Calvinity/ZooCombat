c1e8b56153c34aa2760443faf4a034d39ff8f35b
perme <perme@TAURUS-25>
2026-01-27 00:19:03 +0100

    started on animal class and interface for animalsounds..feel lost.

---
+++ ZooCombat/Animals/Animalclass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooCombat.Animals
+{
+    internal class ClsAnimal
+    {
+        string animalName;
+        int animalHP, animalAP, animalSpeed, animalAttack;
+
+        public ClsAnimal(string animalName, int animalHP, int animalAP, int animalSpeed, int animalAttack)
+        {
+            this.animalName = animalName;
+            this.animalHP = animalHP;
+            this.animalAP = animalAP;
+            this.animalSpeed = animalSpeed;
+            this.animalAttack = animalAttack;
+        }
+    }
+}
---
+++ ZooCombat/Animals/IAnimalSound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooCombat.Animals
+{
+    internal interface IAnimalSound
+    {
+        // Kolla upp hur jag gör interface, borde behöva välja  hur jag importerar animalinfo från databasen?
+        //string animalSound 
+        //{  
+        //    get; 
+        //    set {}; 
+        //}
+    }
+}
